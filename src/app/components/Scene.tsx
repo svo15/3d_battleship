536eb2d1ab1451c38f519dcb791d787969c1cbb1
svo15 <ignukas2007@gmail.com>
2025-04-06 18:24:54 +0300

    removed useless class

--- src/app/components/Scene.tsx
+++ src/app/components/Scene.tsx
@@ -5,20 +5,6 @@ import * as THREE from "three"
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { Sky, Water } from "three/examples/jsm/Addons.js";
 
-class Tile extends THREE.Mesh{
-    _active: boolean;
-    _cubeSize:number;
-    constructor() {
-        super()
-        this._cubeSize=1
-        this._active=false
-        this.geometry = new THREE.BoxGeometry(this._cubeSize,0.1,this._cubeSize)
-        this.material = new THREE.MeshStandardMaterial({ color: "white"})
-        
-        
-        this.position.set(this._cubeSize+1,this._cubeSize/2,0)
-    }
-}
 
 export default function Scene(){
     const mountRef = useRef<any>(null)
@@ -42,19 +28,20 @@ export default function Scene(){
 
         
         const control = new OrbitControls(camera,render.domElement)
-
         camera.position.set(0,5,20)
         control.update()
 
-        const cellSize=1
-        const Gridsize=14
-        const cells:any=[]
+       
 
 
 
         const gridGroup=new THREE.Group()
         gridGroup.rotation.x=Math.PI*0.5
-        scene.add(gridGroup)
+        scene.add(gridGroup) 
+        
+        const cellSize=1
+        const Gridsize=14
+        const cells:any=[]
 
         const geometryTile=new THREE.BoxGeometry(cellSize,0.001)
         const edgeGeometry=new THREE.EdgesGeometry(geometryTile)
